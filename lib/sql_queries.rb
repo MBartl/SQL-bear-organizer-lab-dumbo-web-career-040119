8f73395154f57989469d06d4e1a48cfa183c6ad8
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 19:57:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def selects_all_female_bears_return_name_and_age
   binding.pry
-  "SELECT * FROM bears WHERE gender = 'F';"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
