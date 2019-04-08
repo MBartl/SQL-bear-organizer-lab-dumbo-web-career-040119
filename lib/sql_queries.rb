be5a6cfb5d6d72dec7ea5ccb06a53f7a9ecaf0c1
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 20:31:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,6 +30,6 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "UPDATE bears "
+  "UPDATE bears SET alive = 0"
   "SELECT * FROM bears WHERE temperament = 'aggressive';"
 end
